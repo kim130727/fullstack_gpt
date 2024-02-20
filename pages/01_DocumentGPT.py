1850e54cdbc79a77d3e7030b9fa0d686707f74fe
kim130727 <kim130727@gmail.com>
2024-02-20 18:35:26 +0900

    7.7 Chat History

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -11,21 +11,7 @@ st.set_page_config(
     page_icon="📃",
 )
 
-st.title("DocumentGPT")
-
-st.markdown(
-    """
-Welcome!
-            
-Use this chatbot to ask questions to an AI about your files!
-"""
-)
-
-file = st.file_uploader(
-    "Upload a .txt .pdf or .docx file",
-    type=["pdf", "txt", "docx"],
-)
-
+@st.cache_resource(show_spinner="Embedding file...") #st.cache_data가 안되서 바꿔보았음
 def embed_file(file):
     file_content = file.read()
     file_path = f"./.cache/files/{file.name}"
@@ -45,7 +31,46 @@ def embed_file(file):
     retriever = vectorstore.as_retriever()
     return retriever
 
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.markdown(message)
+    if save:
+        st.session_state["messages"].append({"message": message, "role": role})
+
+
+def paint_history():
+    for message in st.session_state["messages"]:
+        send_message(
+            message["message"],
+            message["role"],
+            save=False,
+        )
+
+st.title("DocumentGPT")
+
+st.markdown(
+    """
+Welcome!
+            
+Use this chatbot to ask questions to an AI about your files!
+
+Upload your files on the sidebar.
+"""
+)
+
+with st.sidebar:
+    file = st.file_uploader(
+        "Upload a .txt .pdf or .docx file",
+        type=["pdf", "txt", "docx"],
+    )
+
 if file:
     retriever = embed_file(file)
-    s = retriever.invoke("winston")
-    s
+    send_message("I'm ready! Ask away!", "ai", save=False)
+    paint_history()
+    message = st.chat_input("Ask anything about your file...")
+    if message:
+        send_message(message, "human")
+        send_message("whats up human", "ai")
+else:
+    st.session_state["messages"] = []

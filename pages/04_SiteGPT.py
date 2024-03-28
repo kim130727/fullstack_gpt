ed76d7c3dfbd1e78f9641f6020da036be48b7318
kim130727 <kim130727@gmail.com>
2024-03-28 17:47:14 +0900

    10.3 Parsing Function

--- pages/04_SiteGPT.py
+++ pages/04_SiteGPT.py
@@ -1,20 +1,46 @@
-from langchain.document_loaders import SitemapLoader
+from langchain.document_loaders import SitemapLoader, text
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 import streamlit as st
-import asyncio
 
-asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
+
+def parse_page(soup):
+    header = soup.find("header")
+    footer = soup.find("footer")
+    if header:
+        header.decompose()
+    if footer:
+        footer.decompose()
+    return (
+        str(soup.get_text())
+        .replace("\n", " ")
+        .replace("\xa0", " ")
+        .replace("CloseSearch Submit Blog", "")
+    )
+
 
 @st.cache_data(show_spinner="Loading website...")
 def load_website(url):
-    loader = SitemapLoader(url)
-    loader.requests_per_second = 5
-    docs = loader.load()
+    splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=1000,
+        chunk_overlap=200,
+    )
+    loader = SitemapLoader(
+        url,
+        filter_urls=[
+            r"^(.*\/blog\/).*",
+        ],
+        parsing_function=parse_page,
+    )
+    loader.requests_per_second = 2
+    docs = loader.load_and_split(text_splitter=splitter)
     return docs
+
 
 st.set_page_config(
     page_title="SiteGPT",
     page_icon="🖥️",
 )
+
 
 st.markdown(
     """

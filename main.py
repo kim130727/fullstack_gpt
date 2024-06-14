86d82ec57707213659c3e11c9052bc62be3642cb
kim130727 <kim130727@gmail.com>
2024-06-15 00:22:50 +0900

    13.2 FastAPI Server

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
-import tiktoken
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
-print (tiktoken)
+app = FastAPI(
+    title="Nicolacus Maximus Quote Giver",
+    description="Get a real quote said by Nicolacus Maximus himself.",
+)
 
-print ("hello")
+
+class Quote(BaseModel):
+    quote: str = Field(
+        description="The quote that Nicolacus Maximus said.",
+    )
+    year: int = Field(
+        description="The year when Nicolacus Maximus said the quote.",
+    )
+
+
+@app.get(
+    "/quote",
+    summary="Returns a random quote by Nicolacus Maximus",
+    description="Upon receiving a GET request this endpoint will return a real quiote said by Nicolacus Maximus himself.",
+    response_description="A Quote object that contains the quote said by Nicolacus Maximus and the date when the quote was said.",
+    response_model=Quote,
+)
+def get_quote():
+    return {
+        "quote": "Life is short so eat it all.",
+        "year": 1950,
+    }
